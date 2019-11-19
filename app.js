19d0b0ff1e65c937d4ced8402f1cfc378ef32ddd
mhdiiilham <muhd.iiilham@gmail.com>
2019-11-19 17:30:50 +0700

    partials

--- app.js
+++ app.js
@@ -8,5 +8,6 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
 
+app.use('/user', User) // route User
 
 app.listen(PORT, () => { console.log(`Listening on port ${PORT}!`)});
--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,18 @@
 const { User } = require('../models')
 
 class userController {
-
+    static register(req, res) { // showing register page
+        res.render('./user/register')
+    }
+    static create(req, res) {
+        User.create(req.body)
+            .then((user)=> {
+                res.send(user)
+            })
+            .catch(err=> {
+                res.send(err);
+            });
+    }
 }
 
 module.exports = userController
