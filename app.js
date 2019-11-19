02ef50508b94eb747e993488932230b2e4db9294
mhdiiilham <muhd.iiilham@gmail.com>
2019-11-19 18:12:32 +0700

    homepage ongoing

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const { User, Item, Category } = require('./routers')
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/', (req, res)=> {
+    res.render('hompage/index')
+})
 app.use('/user', User) // route User
 app.use('/item', Item) // route Item - Admin
 app.use('/category', Category) // route Category - Admin
