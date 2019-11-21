f97d0c2f31db846a55a933bcd7ec9fc6409d2f33
sukmabrahmantya <brahmantyasukma@gmail.com>
2019-11-21 20:39:19 +0700

    fix node mailer

--- helpers/nodemailer.js
+++ helpers/nodemailer.js
@@ -1,20 +1,18 @@
 const nodemailer = require('nodemailer');
-function sendEmail(email, data){
-    let { item, nominal } = data
+function sendEmail(email, item, nominal){
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-               user: `lelang.noreply@gmail.com`,
-               pass: `lelangElang1`
+               user: `slimyhebat@gmail.com`,
+               pass: `NabilaR6`
            }
        });
     
        const mailOptions = {
         from: `lelangElang1@gmail.com`,
         to: `${email}`,
-        subject: 'Selamat!',
-        html: `
-        <h1>Selamat!</h1>
+        subject: 'Please Confirm Your Email!',
+        html: `<h1>Selamat!</h1>
         <h2>Anda dinyatakan memang dalam pelelangan ${item} dengan nominal: ${nominal}</h2>
         <h2>Tapi boong, karena kita tidak punya item tersebut. HAHAHA!</h2>
         <img src="https://photo-invivo.com/wp-content/uploads/2019/03/crying-laughing-meme-wonderfully-funny-laughing-and-crying-face-memes-mojly-of-crying-laughing-meme.jpg" width="400" height="400">
@@ -28,3 +26,5 @@ function sendEmail(email, data){
      });
 }
 module.exports = sendEmail;
+
+
